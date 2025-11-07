bf96d260701926d6caf12adfc46be293ef385c1e
hunghd <hunghd@rikkeisoft.com>
2025-11-07 11:35:56 +0700

    #

--- forge.config.js
+++ forge.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    packagerConfig: {
-      icon: 'images/icon',
-    },
-    // makers: [
-    //     {
-    //         name: '@electron-forge/maker-deb',
-    //         config: {
-    //           options: {
-    //             icon: 'images/icon.png'
-    //           }
-    //         }
-    //     }
-    // ]
+    // packagerConfig: {
+    //   icon: 'images/icon',
+    // },
+    makers: [
+        {
+            name: '@electron-forge/maker-deb',
+            config: {
+              options: {
+                icon: 'images/icon.png'
+              }
+            }
+        }
+    ]
   };
