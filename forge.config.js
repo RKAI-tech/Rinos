a967aed7494e598921c59e331d443e59046a9933
HungHoangDinh <hunghd20012003@gmail.com>
2025-11-07 11:00:46 +0700

    #

--- forge.config.js
+++ forge.config.js
@@ -2,14 +2,14 @@ module.exports = {
     packagerConfig: {
       icon: 'images/icon',
     },
-    makers: [
-        {
-            name: '@electron-forge/maker-deb',
-            config: {
-              options: {
-                icon: 'images/icon.png'
-              }
-            }
-        }
-    ]
+    // makers: [
+    //     {
+    //         name: '@electron-forge/maker-deb',
+    //         config: {
+    //           options: {
+    //             icon: 'images/icon.png'
+    //           }
+    //         }
+    //     }
+    // ]
   };
