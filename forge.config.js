d0e8572f505ffa76c8ea95243df7676b6aec2bfb
hunghd <hunghd@rikkeisoft.com>
2025-12-05 11:36:48 +0700

    3

--- forge.config.js
+++ forge.config.js
@@ -1,18 +1,18 @@
 module.exports = {
     //if win
-     packagerConfig: {
-       icon: 'images/icon',
-     },
+    //  packagerConfig: {
+    //    icon: 'images/icon',
+    //  },
     //for limux 
-    // makers: [
-    //     {
-    //         name: '@electron-forge/maker-deb',
-    //         config: {
-    //           options: {
-    //             icon: 'images/icon.png'
-    //           }
-    //         }
-    //     }
-    // ]
+    makers: [
+        {
+            name: '@electron-forge/maker-deb',
+            config: {
+              options: {
+                icon: 'images/icon.png'
+              }
+            }
+        }
+    ]
     
   };
